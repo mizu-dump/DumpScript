ba3ee11566841d8d59633a699031619def21fbc3
𝐌 𝐈 𝐙 𝐔 𝐌 𝐀 𝐑 𝐔 <mizumaruofficialdump@gmail.com>
2025-12-15 16:24:22 +0800

    Update loadser.lua

--- loadser.lua
+++ loadser.lua
@@ -1,2 +1,2 @@
 --[[ MizuHub | Support 7 Games, More Soon!...]]
-loadstring(game:HttpGet("https://raw.githubusercontent.com/mizu-dump/MizuHub/refs/heads/main/Loader.lua"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/0598fe85aebe3417c0a2b26d2888153f47c073c97a3cc404a6fbc357ef64e2b0/download"))()
